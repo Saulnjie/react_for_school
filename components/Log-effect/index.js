646be83689cb91b65d08d2823df84e43eba80150
SAUL NJIE <SAULUS92@gmail.com>
2022-01-18 11:07:07 +0100

    Loading and Axios I

--- components/Log-effect/index.js
+++ components/Log-effect/index.js
@@ -3,15 +3,27 @@ import React, {useEffect, useState} from "react";
 
 const People = () => {
     const [people, setPeople] = useState ([]);
+    const [isLoading, setIsLoading] = useState(false);
+    // const [isError, setIsError] = useState();
   
     useEffect(() => {
         async function fetchData() {
+            setIsLoading(true);
+
             const response = await fetch ("https://reqres.in/api/users")
             const json = await response.json();
             setPeople(json.data);
+
+            setIsLoading(false);
         }
         fetchData();
     }, []);
+
+
+    if (isLoading) {
+        return <h2>Loading</h2>
+        return console.log("ITS LOADING")
+    };
 
     return (
         <div>
